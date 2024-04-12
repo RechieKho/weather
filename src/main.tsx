b3737756e67120a1454daf950c0bf62616125444
RechieKho <the.real.richie.kho@gmail.com>
2024-04-12 14:48:22 +0800

    Setup project.

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import "./reset.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "./index.css";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Typography } from "@mui/material";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Typography>Hello world!</Typography>
+  </React.StrictMode>
+);
