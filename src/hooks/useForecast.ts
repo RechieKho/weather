de9868eb60b5378fadcff42040c2fd0bf877cb1a
RechieKho <the.real.richie.kho@gmail.com>
2024-04-12 21:34:02 +0800

    Add `useForcast`.

---
+++ src/hooks/useForecast.ts
@@ -0,0 +1,103 @@
+import { DependencyList, useEffect, useState } from "react";
+import { z } from "zod";
+
+const forcastURL = "https://api.openweathermap.org/data/2.5/forecast";
+
+const Forcast = z.object({
+  cod: z.number(),
+  message: z.string(),
+  cnt: z.number().positive().int().finite(),
+  list: z.array(
+    z.object({
+      dt: z.number().positive().int().finite(),
+      main: z.object({
+        temp: z.number().finite(),
+        feels_like: z.number().finite(),
+        temp_min: z.number().finite(),
+        temp_max: z.number().finite(),
+        pressure: z.number().positive().finite(),
+        humidity: z.number().min(0).max(100),
+        sea_level: z.number().positive().finite(),
+        grnd_level: z.number().positive().finite(),
+        temp_kf: z.number(),
+      }),
+      weather: z.object({
+        id: z.number(),
+        main: z.string(),
+        description: z.string(),
+        icon: z.string(),
+      }),
+      city: z.object({
+        coord: z.object({
+          lat: z.number().finite(),
+          lon: z.number().finite(),
+        }),
+        population: z.number().positive().finite(),
+        timezone: z.number().positive().int(),
+        sunrise: z.number().positive().int(),
+        sunset: z.number().positive().int(),
+      }),
+    })
+  ),
+});
+
+export default function useForcast({
+  apiKey,
+  latitude,
+  longitude,
+  units = "metric",
+  language = "EN",
+  count = 3,
+  deps,
+}: {
+  apiKey: string;
+  latitude: number;
+  longitude: number;
+  units: string;
+  language: string;
+  count: number;
+  deps?: DependencyList;
+}) {
+  const [forcast, setForcast] = useState<
+    z.infer<typeof Forcast> | Error | null
+  >(null);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    (async () => {
+      try {
+        const headers = new Headers();
+        const searchParams = new URLSearchParams();
+        searchParams.append("lat", latitude.toString());
+        searchParams.append("lon", longitude.toString());
+        searchParams.append("appid", apiKey);
+        searchParams.append("units", units);
+        searchParams.append("mode", "json");
+        searchParams.append("cnt", count.toString());
+        searchParams.append("lang", language);
+
+        const request = new Request(
+          `${forcastURL}?${searchParams.toString()}`,
+          {
+            method: "GET",
+            headers,
+            mode: "cors",
+            cache: "default",
+          }
+        );
+
+        const result = fetch(request, { signal: controller.signal });
+        setForcast(Forcast.parse((await result).json()));
+      } catch (e) {
+        if (!(e instanceof Error)) return; // Ignore malformed throw.
+        if (e.name === "AbortError") return; // Ignore aboort error.
+        setForcast(e);
+      }
+    })();
+
+    return () => controller.abort();
+  }, deps);
+
+  return forcast;
+}
