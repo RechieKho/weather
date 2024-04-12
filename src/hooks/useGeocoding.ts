72a5625a23467f0b5cd43f11f8fee23d3cc03b27
RechieKho <the.real.richie.kho@gmail.com>
2024-04-12 21:46:32 +0800

    Add `useGeocoding`.

---
+++ src/hooks/useGeocoding.ts
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react";
+import { z } from "zod";
+
+const geocodingURL = "https://api.openweathermap.org/geo/1.0/direct";
+
+const Geocoding = z.array(
+  z.object({
+    name: z.string(),
+    lat: z.number().finite(),
+    lon: z.number().finite(),
+    country: z.string(),
+    state: z.string(),
+  })
+);
+
+export default function useGeocoding({
+  apiKey,
+  query,
+  limit = 5,
+}: {
+  apiKey: string;
+  query: string;
+  limit: number;
+}) {
+  const [geocoding, setGeocoding] = useState<
+    z.infer<typeof Geocoding> | Error | null
+  >();
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    (async () => {
+      try {
+        const headers = new Headers();
+        const searchParams = new URLSearchParams();
+        searchParams.append("q", query);
+        searchParams.append("appid", apiKey);
+        searchParams.append("limit", limit.toString());
+
+        const request = new Request(
+          `${geocodingURL}?${searchParams.toString()}`,
+          {
+            method: "GET",
+            headers,
+            mode: "cors",
+            cache: "default",
+          }
+        );
+
+        const result = await fetch(request, { signal: controller.signal });
+        setGeocoding(Geocoding.parse(await result.json()));
+      } catch (e) {
+        if (!(e instanceof Error)) return; // Ignore malformed throw.
+        if (e.name === "AbortError") return; // Ignore abort error.
+        setGeocoding(e);
+      }
+    })();
+
+    return () => controller.abort();
+  }, [apiKey, query, limit]);
+
+  return geocoding;
+}
